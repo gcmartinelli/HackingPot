9fd4a971ddea08699bfc921c65ab14e3078a5856
Giancarlo Martinelli <gcmartinelli@gmail.com>
2012-05-20 16:59:18 -0300

    corrected some spelling errors and an empty search query 500 error

--- searchengine/views.py
+++ searchengine/views.py
@@ -4,13 +4,44 @@ from django.shortcuts import render_to_response
 
 def search(request):
 	query = request.GET.get('q', '')
-	print query
 	query = query.split(',')
 	q = []
 	for entry in query:
-		s = entry.lower()
-		while s[0] == " ":
-			s = s[1:]
-		q.append(s)
-	results = searchquery(q)
+		l = len(entry)
+		i = 0
+		if l > 0:
+			while i < l:
+				if entry[i] == " ":
+					i += 1
+			if i == l:
+				results = None
+				query = None
+				return render_to_response('search.html', dict(results=results, query=query))
+			while entry[0] == " ":
+				entry = entry[1:]
+			s = entry.lower()
+			q.append(s)
+		results = None
+		query = None
+		return render_to_response('search.html', dict(results=results, query=query))
+	results = searchquery(s)
 	return render_to_response('search.html', dict(results=results, query=query))
+
+
+
+
+#def search(request):
+#	query = request.GET.get('q', '')
+#	if len(query) > 0 and query != " ":
+#		query = query.split(',')
+#		q = []
+#		for entry in query:
+#			s = entry.lower()
+#			while s[0] == " ":
+#				s = s[1:]
+#			q.append(s)
+#		results = searchquery(q)
+#		return render_to_response('search.html', dict(results=results, query=query))
+#	else:
+#		results = None
+#		return render_to_response('search.html', dict(results=results, query=query))
