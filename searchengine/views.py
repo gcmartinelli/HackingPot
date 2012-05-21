dea39d558aa23339f892eb4602254e797b8851ad
Giancarlo Martinelli <gcmartinelli@gmail.com>
2012-05-20 21:29:00 -0300

    Corrected 500 error when searching for empty strings.

--- searchengine/views.py
+++ searchengine/views.py
@@ -4,44 +4,13 @@ from django.shortcuts import render_to_response
 
 def search(request):
 	query = request.GET.get('q', '')
-	query = query.split(',')
+	query = query.strip()
 	q = []
-	for entry in query:
-		l = len(entry)
-		i = 0
-		if l > 0:
-			while i < l:
-				if entry[i] == " ":
-					i += 1
-			if i == l:
-				results = None
-				query = None
-				return render_to_response('search.html', dict(results=results, query=query))
-			while entry[0] == " ":
-				entry = entry[1:]
+	if query:
+		query = query.split(',')
+		for entry in query:
 			s = entry.lower()
 			q.append(s)
-		results = None
-		query = None
+		results = searchquery(q)
 		return render_to_response('search.html', dict(results=results, query=query))
-	results = searchquery(s)
-	return render_to_response('search.html', dict(results=results, query=query))
-
-
-
-
-#def search(request):
-#	query = request.GET.get('q', '')
-#	if len(query) > 0 and query != " ":
-#		query = query.split(',')
-#		q = []
-#		for entry in query:
-#			s = entry.lower()
-#			while s[0] == " ":
-#				s = s[1:]
-#			q.append(s)
-#		results = searchquery(q)
-#		return render_to_response('search.html', dict(results=results, query=query))
-#	else:
-#		results = None
-#		return render_to_response('search.html', dict(results=results, query=query))
+	return render_to_response('search.html', dict(results=None, query=None))
